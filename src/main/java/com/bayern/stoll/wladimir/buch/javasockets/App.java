3cf7eb816bb0f74461d9c52ff45cc6007de51986
wladi <wladi@LAPTOP-PQFEJC38>
2017-05-01 17:03:39 +0200

    init

---
+++ src/main/java/com/bayern/stoll/wladimir/buch/javasockets/App.java
@@ -0,0 +1,13 @@
+package com.bayern.stoll.wladimir.buch.javasockets;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
